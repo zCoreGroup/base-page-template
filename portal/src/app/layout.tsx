ea40346e2c533e1f0563606ffcb7a65091f3f53c
Stephen Pardue <spardue@zcoregroup.com>
2024-06-27 11:30:35 -0400

    Better Favicon

--- portal/src/app/layout.tsx
+++ portal/src/app/layout.tsx
@@ -15,7 +15,13 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <head />
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/favicon/csite.webmanifest" />
+        <link rel="icon" href="/favicon/favicon.ico" />
+      </head>
       <body className={libreFranklin.className}>
         <ThemeProvider>
           {children}
