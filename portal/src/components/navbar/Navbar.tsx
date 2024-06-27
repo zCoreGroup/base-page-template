1472122f741e00840e5a3ce19d628a6705244c14
Michael Nau <michaelwnau@gmail.com>
2024-06-27 10:02:48 -0500

    add navbar datafetcher

--- portal/src/components/navbar/Navbar.tsx
+++ portal/src/components/navbar/Navbar.tsx
@@ -1,5 +1,4 @@
-// portal/src/components/navbar/Navbar.tsx
-import React from 'react';
+import React from "react";
 import {
   AppBar,
   Toolbar,
@@ -14,50 +13,59 @@ import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 const Navbar: React.FC = () => {
   return (
-    <AppBar position="static" sx={{ backgroundColor: "#303030" }}>
+    <AppBar
+      position="static"
+      sx={{ backgroundColor: "#303030", height: "72px" }}
+    >
       <Toolbar sx={{ justifyContent: "space-between" }}>
         <Box sx={{ display: "flex", alignItems: "center" }}>
           <img
-            src="/path-to-logo.png"
+            src="/assets/guardian_one_logo_wordMark.png"
             alt="Logo"
-            style={{ width: "80px", marginRight: "20px" }}
+            style={{ width: "280.7px", height: "24px", marginRight: "20px" }}
           />
-          <Typography
-            variant="h6"
-            sx={{ color: "#e74c3c", fontWeight: "bold", fontSize: "28px" }}
-          >
-            GUARDIAN ONE
-          </Typography>
         </Box>
+        <Typography
+          variant="h6"
+          sx={{
+            color: "#e74c3c",
+            fontWeight: "bold",
+            fontSize: "28px",
+            textAlign: "center",
+            flexGrow: 1,
+          }}
+        >
+          GUARDIAN ONE
+        </Typography>
         <Box sx={{ display: "flex", alignItems: "center" }}>
           <Typography
             variant="body1"
-            sx={{ color: "#e4e2e3", marginRight: "20px", fontSize: "14px" }}
+            sx={{ color: "#e4e2e3", marginRight: "24px", fontSize: "14px" }}
           >
             Doctrine
           </Typography>
           <Typography
             variant="body1"
-            sx={{ color: "#e4e2e3", marginRight: "20px", fontSize: "14px" }}
+            sx={{ color: "#e4e2e3", marginRight: "24px", fontSize: "14px" }}
           >
             News
           </Typography>
           <Typography
             variant="body1"
-            sx={{ color: "#e4e2e3", marginRight: "20px", fontSize: "14px" }}
+            sx={{ color: "#e4e2e3", marginRight: "24px", fontSize: "14px" }}
           >
             CSO Corner
           </Typography>
           <Typography
             variant="body1"
-            sx={{ color: "#e4e2e3", marginRight: "20px", fontSize: "14px" }}
+            sx={{ color: "#e4e2e3", marginRight: "24px", fontSize: "14px" }}
           >
             Multimedia
           </Typography>
-          <IconButton sx={{ color: "#e4e2e3" }}>
+          <IconButton sx={{ color: "#e4e2e3", marginRight: "8px" }}>
             <SearchIcon />
           </IconButton>
-          <IconButton sx={{ color: "#e4e2e3" }}>
+          <IconButton sx={{ color: "#e4e2e3", marginRight: "8px" }}>
             <Badge badgeContent={4} color="error">
               <NotificationsIcon />
             </Badge>
