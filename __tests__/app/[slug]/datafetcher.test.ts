27895306022fe5a8453c6c8fe351305fc3d774aa
Stephen Pardue <spardue@zcoregroup.com>
2024-07-29 15:23:23 -0400

    Update footer test

--- __tests__/app/[slug]/datafetcher.test.ts
+++ __tests__/app/[slug]/datafetcher.test.ts
@@ -40,26 +40,19 @@ mockBannerDataFetcher.fetch = jest.fn().mockResolvedValue({
   visionText: '',
 })
 mockFooterDataFetcher.fetch = jest.fn().mockResolvedValue({
-  title1: '',
   streetAddress: '',
-  cityState: '',
+  city: '',
+  state: '',
+  zip: '',
   phone: '',
   email: '',
-  phone1: '',
-  email1: '',
-  title2: '',
-  hoursMon: '',
-  hoursSat: '',
-  hoursSun: '',
-  hours2: '',
-  hours3: '',
-  title3: '',
-  feedback: '',
-  title4: '',
+  informationTitle: '',
+  informationText: '',
   linkFB: '',
   linkX: '',
   linkIG: '',
   linkYT: '',
+  baseMapImage: '',
 })
 mockFeaturedLinksDataFetcher.fetch = jest.fn().mockResolvedValue({ links: [] })
 mockAnnouncementsDataFetcher.fetch = jest.fn().mockResolvedValue({ articles: [] })
@@ -116,26 +109,19 @@ describe('LandingPageDataFetcher', () => {
       announcements: { articles: [] },
       events: { events: [] },
       footer: {
-        title1: '',
         streetAddress: '',
-        cityState: '',
+        city: '',
+        state: '',
+        zip: '',
         phone: '',
         email: '',
-        phone1: '',
-        email1: '',
-        title2: '',
-        hoursMon: '',
-        hoursSat: '',
-        hoursSun: '',
-        hours2: '',
-        hours3: '',
-        title3: '',
-        feedback: '',
-        title4: '',
+        informationTitle: '',
+        informationText: '',
         linkFB: '',
         linkX: '',
         linkIG: '',
         linkYT: '',
+        baseMapImage: '',
       },
     } as LandingPageData)
 
@@ -160,7 +146,8 @@ describe('LandingPageDataFetcher', () => {
       title: 'Valid Title',
     })
     expect(mockFooterDataFetcher.fetch).toHaveBeenCalledWith({
-      landingPageId: 'valid-slug',
+      slug: 'valid-slug',
+      title: 'Valid Title',
     })
   })
 })
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import { Grid, Typography, Box, Link } from '@mui/material';
-import { Facebook, X, Instagram, YouTube } from '@mui/icons-material';
-import { FooterData } from "@/types";
-import { content, contentTitle, footerContentStyle } from './style';
+import React from 'react'
+import { Grid, Typography, Box, Link } from '@mui/material'
+import { Facebook, X, Instagram, YouTube } from '@mui/icons-material'
+import { FooterData } from '@/types'
+import { content, contentTitle, footerContentStyle } from './style'
 
 const Footer: React.FC<{ data: FooterData }> = ({ data }) => {
-  const informationText = data.informationText.split("\n");
-  const addressText = encodeURIComponent(data.streetAddress + ', ' + data.city + ', ' + data.state + ' ' + data.zip);
+  const informationText = data.informationText.split('\n')
+  const addressText = encodeURIComponent(data.streetAddress + ', ' + data.city + ', ' + data.state + ' ' + data.zip)
   const addressHref = `https://www.google.com/maps/search/?api=1&query=${addressText}`
 
   return (
     <Box sx={footerContentStyle}>
-      <Grid container spacing={4} direction="row" justifyContent="center" alignItems="flex-start">
+      <Grid container spacing={4} direction='row' justifyContent='center' alignItems='flex-start'>
         <Grid item xs={12} sm={6} md={3}>
-          <Grid container direction="column" spacing={2}>
+          <Grid container direction='column' spacing={2}>
             <Grid item>
-              <Box display="flex" flexDirection="column" alignItems="flex-start" height="100%">
-                <Typography variant="h6" gutterBottom sx={contentTitle}>
+              <Box display='flex' flexDirection='column' alignItems='flex-start' height='100%'>
+                <Typography variant='h6' gutterBottom sx={contentTitle}>
                   Locations
                 </Typography>
-                <Link href={addressHref} target="_blank" color="inherit" underline="none">
+                <Link href={addressHref} target='_blank' color='inherit' underline='none'>
                   <Typography flexGrow={1} sx={content}>
                     {data.streetAddress}
                   </Typography>
@@ -28,18 +28,18 @@ const Footer: React.FC<{ data: FooterData }> = ({ data }) => {
                   </Typography>
                 </Link>
                 <Typography flexGrow={1} sx={content}>
-                  <Link href={`mailto:${data.email}`} color="inherit" underline="none">
+                  <Link href={`mailto:${data.email}`} color='inherit' underline='none'>
                     {data.email}&nbsp;
                   </Link>
-                  <Link href={`tel:${data.phone}`} color="inherit" underline="none">
+                  <Link href={`tel:${data.phone}`} color='inherit' underline='none'>
                     {data.phone}
                   </Link>
                 </Typography>
               </Box>
             </Grid>
             <Grid item>
-              <Box display="flex" flexDirection="column" alignItems="flex-start" height="100%">
-                <Typography variant="h6" gutterBottom sx={contentTitle}>
+              <Box display='flex' flexDirection='column' alignItems='flex-start' height='100%'>
+                <Typography variant='h6' gutterBottom sx={contentTitle}>
                   {data.informationTitle}
                 </Typography>
                 {informationText.map((text) => (
@@ -53,30 +53,30 @@ const Footer: React.FC<{ data: FooterData }> = ({ data }) => {
         </Grid>
 
         <Grid item xs={12} sm={6} md={6}>
-          <Box display="flex" flexDirection="column" alignItems="flex-start" height="100%">
-            <Typography variant="h6" gutterBottom sx={contentTitle}>
+          <Box display='flex' flexDirection='column' alignItems='flex-start' height='100%'>
+            <Typography variant='h6' gutterBottom sx={contentTitle}>
               Base Map
             </Typography>
-            <Box component="img" src={data.baseMapImage} width="50%" height="auto" alt="Base Map"/>
+            <Box component='img' src={data.baseMapImage} width='50%' height='auto' alt='Base Map' />
           </Box>
         </Grid>
 
         <Grid item xs={12} sm={6} md={3}>
-          <Box display="flex" flexDirection="column" alignItems="flex-start" height="100%">
-            <Typography variant="h6" gutterBottom sx={contentTitle}>
+          <Box display='flex' flexDirection='column' alignItems='flex-start' height='100%'>
+            <Typography variant='h6' gutterBottom sx={contentTitle}>
               Get Connected
             </Typography>
             <Box sx={{ display: 'flex', gap: 1 }}>
-              <Link href={data.linkFB} target="_blank" color="inherit">
+              <Link href={data.linkFB} target='_blank' color='inherit'>
                 <Facebook />
               </Link>
-              <Link href={data.linkX} target="_blank" color="inherit">
+              <Link href={data.linkX} target='_blank' color='inherit'>
                 <X />
               </Link>
-              <Link href={data.linkIG} target="_blank" color="inherit">
+              <Link href={data.linkIG} target='_blank' color='inherit'>
                 <Instagram />
               </Link>
-              <Link href={data.linkYT} target="_blank" color="inherit">
+              <Link href={data.linkYT} target='_blank' color='inherit'>
                 <YouTube />
               </Link>
             </Box>
@@ -84,7 +84,7 @@ const Footer: React.FC<{ data: FooterData }> = ({ data }) => {
         </Grid>
       </Grid>
     </Box>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
