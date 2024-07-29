77ebfe5aa5e65b0f49c7fb648500554efd031377
Nate Muesing <nmuesing@clarityinnovates.com>
2024-07-29 11:34:36 -0400

    Set up jest for coverage

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,41 @@
 // jest.config.js
+const customJestConfig = {
+  roots: ['<rootDir>', '<rootDir>/src/'],
+  testEnvironment: 'jest-environment-jsdom', // TODO: testing this should fix some issues
+  modulePathIgnorePatterns: [
+    '/node_modules/',
+    '.next/',
+    'migrations/',
+    '/scripts/',
+    'public/',
+    'src/designtokens/',
+    '.storybook/',
+    'src/stories/',
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  watchPathIgnorePatterns: ['globalconfig'],
+  setupFiles: [],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputDirectory: 'test-results',
+        outputName: 'jest-results.xml',
+      },
+    ],
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts', 'src/**/*.tsx', 'startup/*.js'],
+  coverageDirectory: 'coverage',
+  testTimeout: 30000, //30 seconds
+};
+
 const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
   dir: './',
 });
-
-const customJestConfig = {
-  setupFilesAfterEnv: ['./jest.setup.js'],
-  testEnvironment: 'jest-environment-jsdom',
-  testTimeout: 30000, //30 seconds
-};
 
 module.exports = createJestConfig(customJestConfig);
