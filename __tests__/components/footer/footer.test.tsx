9679de5212ff3a090b8cfc50ae0256df98c87690
Stephen Pardue <spardue@zcoregroup.com>
2024-07-30 16:28:41 -0400

    Update test

--- __tests__/components/footer/footer.test.tsx
+++ __tests__/components/footer/footer.test.tsx
@@ -12,14 +12,15 @@ describe('Footer', () => {
     phone: 'tel:+1(805)606-1110',
     email: 'mailto:SLD30.PA.Workflow@us.af.mil',
     informationTitle: 'Hours of Operations',
-    informationText: 'Mon-Fri: 6am-6pm\nSat: 8am-4pm\nSun: 10am-4pm\nHoliday Closures: Thanksgiving Day and Christmas Day\nAfter Hour Passes: Visitors needing a visitor pass after hours can receive a 24 hour pass at the Santa Maria gate, (located on California Blvd. at the intersection with HWY1)',
+    informationText:
+      'Mon-Fri: 6am-6pm\nSat: 8am-4pm\nSun: 10am-4pm\nHoliday Closures: Thanksgiving Day and Christmas Day\nAfter Hour Passes: Visitors needing a visitor pass after hours can receive a 24 hour pass at the Santa Maria gate, (located on California Blvd. at the intersection with HWY1)',
     baseMapImage: 'path/to/baseMapImage.jpg',
     linkFB: 'https://www.facebook.com/SLD30/',
     linkX: 'https://x.com/sldelta30',
     linkIG: 'https://www.instagram.com/vandenberg_sfb/',
     linkYT: 'https://www.youtube.com/user/30SWVandenberg',
     quickLinks: [],
-    guardianPortal: []
+    guardianPortal: [],
   }
 
   it('renders the titles, subtitles, and links correctly', () => {
